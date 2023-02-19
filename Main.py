683bb7c05e191caeecc1e4b06a9736a0cfab8788
NikitaFranks <124629788+NikitaFranks@users.noreply.github.com>
2023-02-19 20:28:53 +0200

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,30 @@
+from collections import namedtuple
+
+Bracket = namedtuple("Bracket", ["char", "position"])
+
+
+def are_matching(left, right):
+    return (left + right) in ["()", "[]", "{}"]
+
+
+def find_mismatch(text):
+    opening_brackets_stack = []
+    for i, next in enumerate(text):
+        if next in "([{":
+            opening_brackets_stack.append(Bracket(next, i+1))
+        if next in ")]}":
+            if len(opening_brackets_stack) == 0:
+                return i+1
+            top = opening_brackets_stack.pop()
+            if not are_matching(top.char, next):
+                return i+1
+    if len(opening_brackets_stack) > 0:
+        return opening_brackets_stack[0].position
+    else:
+        return "Success"
+
+
+def main():
+    text = input()
+    mismatch = find_mismatch(text)
+    print(mismatch)
